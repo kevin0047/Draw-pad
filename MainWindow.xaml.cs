e3216ccdbb95d01e7c5da1f0788a132c297f7b96
kevin0047 <ybnam@devlink.co.kr>
2024-08-26 09:43:05 +0900

    선긋기 기능과 클리어 기능

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,53 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Draw_pad
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isDrawing;
+        private Polyline currentLine;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                isDrawing = true;
+                currentLine = new Polyline
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2
+                };
+                paintCanvas.Children.Add(currentLine);
+                Point point = e.GetPosition(paintCanvas);
+                currentLine.Points.Add(point);
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Point point = e.GetPosition(paintCanvas);
+                currentLine.Points.Add(point);
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            paintCanvas.Children.Clear();
         }
     }
 }
